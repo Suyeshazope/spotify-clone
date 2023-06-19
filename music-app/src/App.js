265026e9fb079a5ad11debcbc913a921d8721357
Suyeshazope <96578526+Suyeshazope@users.noreply.github.com>
2023-06-19 17:56:02 +0530

    intial commit

---
+++ music-app/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import { LeftMenu } from './components/LeftMenu';
+import { RightMenu } from './components/RightMenu';
+import { MainContainer } from './components/MainContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <LeftMenu />
+      <MainContainer />
+      <RightMenu />
+      <div className='background'> </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ music-app/src/components/PlayList.js
@@ -0,0 +1,10 @@
+const PlayList = [
+    { id: 1, name: "Top hit 2021" },
+    { id: 2, name: "Dance deewane" },
+    { id: 3, name: "Bollywood Mush" },
+    { id: 4, name: "Hot hits" },
+    { id: 5, name: "Instrumental" },
+    { id: 6, name: "Daily Mix" }
+]
+
+export {PlayList} ;
